54ed05da5637ca34fab5303c8f14e263a308c93d
harrieschen <harrieschen@gmail.com>
2017-08-03 17:38:55 +0200

    add devise scale

--- lib/spree/advanced_report/scale_report/device_scale.rb
+++ lib/spree/advanced_report/scale_report/device_scale.rb
@@ -1,15 +1,32 @@
 class Spree::AdvancedReport::ScaleReport::DeviceScale < Spree::AdvancedReport
   def name
-    "Profit by Geography"
+    'Device scale'
   end
 
-
   def description
-    "Profit divided geographically, into states and countries"
+    'Device scale'
   end
 
   def initialize(params)
     super(params)
 
   end
+
+  def report_data
+    @orders = Spree::Order.where('completed_at IS NOT NULL').order('completed_at DESC').limit(100)
+
+    data = [
+      ['platform', 'count']
+    ]
+
+    channels = @orders.group_by(&:channel)
+
+    channels.each_key {|k| puts k}
+
+    channels.each_key do |k|
+      data += [[k, channels[k].count]]
+    end
+    data
+  end
+
 end
